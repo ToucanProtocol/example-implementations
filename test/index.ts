baa960755a140baff4b3822948a117fb21100e20
lazaralex98 <alex2012ro@gmail.com>
2022-02-25 02:24:35 +0200

    tests boilerplate

--- test/index.ts
+++ test/index.ts
@@ -3,11 +3,27 @@ import { expect } from "chai";
 import { ethers, network } from "hardhat";
 import * as tcoAbi from "../artifacts/contracts/CO2KEN_contracts/ToucanCarbonOffsets.sol/ToucanCarbonOffsets.json";
 import * as bctAbi from "../artifacts/contracts/CO2KEN_contracts/pools/BaseCarbonTonne.sol/BaseCarbonTonne.json";
-import * as nctAbi from "../artifacts/contracts/CO2KEN_contracts/pools/NCT.sol/NCT.json";
+import * as nctAbi from "../artifacts/contracts/CO2KEN_contracts/pools/NCT.sol/NatureCarbonTonne.json";
+import {
+  BaseCarbonTonne,
+  OffsetHelper,
+  OffsetHelper__factory,
+  ToucanCarbonOffsets,
+} from "../typechain";
 
-const tco2Address: string = "0xa5831eb637dff307395b5183c86B04c69C518681";
-const myAddress: string = "0x721F6f7A29b99CbdE1F18C4AA7D7AEb31eb2923B";
-const bctAddress: string = "0xf2438A14f668b1bbA53408346288f3d7C71c10a1";
+const tco2Address: string = "";
+const myAddress: string = "";
+const bctAddress: string = "";
+
+const addresses: any = {
+  myAddress: "0x721F6f7A29b99CbdE1F18C4AA7D7AEb31eb2923B",
+  tco2Address: "0xa5831eb637dff307395b5183c86B04c69C518681",
+  bctAddress: "0xf2438A14f668b1bbA53408346288f3d7C71c10a1",
+  nctAddress: "0x7beCBA11618Ca63Ead5605DE235f6dD3b25c530E",
+  usdcAddress: "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174",
+  wethAddress: "0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619",
+  wmaticAddress: "0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270",
+};
 
 describe("Offset Helper", function () {
   let offsetHelper: OffsetHelper;
@@ -30,36 +46,25 @@ describe("Offset Helper", function () {
       });
     }
 
-    // we get a signer based on my above address (I have TCO2, BCT & MATIC on it at the blockNumber I chose)
     owner = await ethers.getSigner(myAddress);
-    // and we get a bunch of other random signers
     [addr1, addr2, ...addrs] = await ethers.getSigners();
 
-    // we deploy a DEX contract and get a portal to it
-    const copFactory = (await ethers.getContractFactory(
-      "ContractOffsetterPOC",
+    const offsetHelperFactory = (await ethers.getContractFactory(
+      "OffsetHelper",
       owner
-      // eslint-disable-next-line camelcase
     )) as OffsetHelper__factory;
-    offsetHelper = await copFactory.deploy();
-
-    // we instantiate a portal to my TCO2 contract
-    // @ts-ignore
-    tco = new ethers.Contract(tco2Address, tcoAbi.abi, owner);
-
-    // we instantiate a portal to BCT
-    // @ts-ignore
-    bct = new ethers.Contract(bctAddress, bctAbi.abi, owner);
+    offsetHelper = await offsetHelperFactory.deploy();
   });
 
   describe("swap()", function () {
     it("Should blah blah", async function () {
-      // example
+      // @ts-ignore
+      bct = new ethers.Contract(bctAddress, bctAbi.abi, owner);
+
       expect("1.0").to.be.eql("0.00000152");
     });
 
     it("Should be reverted with 'blah blah.'", async function () {
-      // example
       await expect("1.0").to.be.revertedWith(
         "You can't offset more than your footprint."
       );
