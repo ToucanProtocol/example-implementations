6d9cd7eb5fb69ab95a8ccfc804ffce249e03be15
lazaralex98 <alex2012ro@gmail.com>
2022-03-10 18:23:30 +0200

    better tests

--- test/index.ts
+++ test/index.ts
@@ -597,7 +597,7 @@ describe("Offset Helper", function () {
   });
 
   describe("autoOffset()", function () {
-    it("Should retire 1 TCO2 from WETH", async function () {
+    it("User's in-contract TCO2 balance should be 0.0 - offset from WETH", async function () {
       // since I have no WETH, I need to impersonate an account that has it
       // I'll also give it some wei just to be safe
       await network.provider.request({
@@ -639,7 +639,6 @@ describe("Offset Helper", function () {
           )
       ).wait();
 
-      // TODO there should be a better more accurate way to test this, like maybe finding the total TCO2 supply
       expect(
         formatEther(
           await offsetHelper.tco2Balance(
@@ -649,7 +648,66 @@ describe("Offset Helper", function () {
       ).to.be.eql("0.0");
     });
 
-    it("Should retire 1 TCO2 from MATIC", async function () {
+    it("OffsetHelpers's TCO2 balances should be 0.0 - offset from WETH", async function () {
+      // since I have no WETH, I need to impersonate an account that has it
+      // I'll also give it some wei just to be safe
+      await network.provider.request({
+        method: "hardhat_stopImpersonatingAccount",
+        params: [addresses.myAddress],
+      });
+      await network.provider.request({
+        method: "hardhat_impersonateAccount",
+        params: ["0xdc9232e2df177d7a12fdff6ecbab114e2231198d"],
+      });
+      await network.provider.send("hardhat_setBalance", [
+        "0xdc9232e2df177d7a12fdff6ecbab114e2231198d",
+        parseEther("2.0").toHexString(),
+      ]);
+      const signer = await ethers.getSigner(
+        "0xdc9232e2df177d7a12fdff6ecbab114e2231198d"
+      );
+
+      const iface = new Interface(
+        '[{"inputs":[{"internalType":"address","name":"childChainManager","type":"address"}],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"spender","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"address","name":"userAddress","type":"address"},{"indexed":false,"internalType":"address payable","name":"relayerAddress","type":"address"},{"indexed":false,"internalType":"bytes","name":"functionSignature","type":"bytes"}],"name":"MetaTransactionExecuted","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"bytes32","name":"role","type":"bytes32"},{"indexed":true,"internalType":"bytes32","name":"previousAdminRole","type":"bytes32"},{"indexed":true,"internalType":"bytes32","name":"newAdminRole","type":"bytes32"}],"name":"RoleAdminChanged","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"bytes32","name":"role","type":"bytes32"},{"indexed":true,"internalType":"address","name":"account","type":"address"},{"indexed":true,"internalType":"address","name":"sender","type":"address"}],"name":"RoleGranted","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"bytes32","name":"role","type":"bytes32"},{"indexed":true,"internalType":"address","name":"account","type":"address"},{"indexed":true,"internalType":"address","name":"sender","type":"address"}],"name":"RoleRevoked","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Transfer","type":"event"},{"inputs":[],"name":"CHILD_CHAIN_ID","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"CHILD_CHAIN_ID_BYTES","outputs":[{"internalType":"bytes","name":"","type":"bytes"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"DEFAULT_ADMIN_ROLE","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"DEPOSITOR_ROLE","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"ERC712_VERSION","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"ROOT_CHAIN_ID","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"ROOT_CHAIN_ID_BYTES","outputs":[{"internalType":"bytes","name":"","type":"bytes"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"spender","type":"address"}],"name":"allowance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"approve","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"account","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"decimals","outputs":[{"internalType":"uint8","name":"","type":"uint8"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"subtractedValue","type":"uint256"}],"name":"decreaseAllowance","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"user","type":"address"},{"internalType":"bytes","name":"depositData","type":"bytes"}],"name":"deposit","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"userAddress","type":"address"},{"internalType":"bytes","name":"functionSignature","type":"bytes"},{"internalType":"bytes32","name":"sigR","type":"bytes32"},{"internalType":"bytes32","name":"sigS","type":"bytes32"},{"internalType":"uint8","name":"sigV","type":"uint8"}],"name":"executeMetaTransaction","outputs":[{"internalType":"bytes","name":"","type":"bytes"}],"stateMutability":"payable","type":"function"},{"inputs":[],"name":"getChainId","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"pure","type":"function"},{"inputs":[],"name":"getDomainSeperator","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"user","type":"address"}],"name":"getNonce","outputs":[{"internalType":"uint256","name":"nonce","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"bytes32","name":"role","type":"bytes32"}],"name":"getRoleAdmin","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"bytes32","name":"role","type":"bytes32"},{"internalType":"uint256","name":"index","type":"uint256"}],"name":"getRoleMember","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"bytes32","name":"role","type":"bytes32"}],"name":"getRoleMemberCount","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"bytes32","name":"role","type":"bytes32"},{"internalType":"address","name":"account","type":"address"}],"name":"grantRole","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bytes32","name":"role","type":"bytes32"},{"internalType":"address","name":"account","type":"address"}],"name":"hasRole","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"addedValue","type":"uint256"}],"name":"increaseAllowance","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"bytes32","name":"role","type":"bytes32"},{"internalType":"address","name":"account","type":"address"}],"name":"renounceRole","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bytes32","name":"role","type":"bytes32"},{"internalType":"address","name":"account","type":"address"}],"name":"revokeRole","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"recipient","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"transfer","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"sender","type":"address"},{"internalType":"address","name":"recipient","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"transferFrom","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"withdraw","outputs":[],"stateMutability":"nonpayable","type":"function"}]'
+      );
+      iface.format(FormatTypes.full);
+
+      const weth = new ethers.Contract(addresses.wethAddress, iface, owner);
+
+      await (
+        await weth
+          .connect(signer)
+          .approve(offsetHelper.address, parseEther("1.0"))
+      ).wait();
+
+      await (
+        await offsetHelper
+          .connect(signer)
+          ["autoOffset(address,address,uint256)"](
+            addresses.wethAddress,
+            addresses.nctAddress,
+            parseEther("1.0")
+          )
+      ).wait();
+
+      const scoredTCO2s = await nct.getScoredTCO2s();
+
+      let tokenContract: ToucanCarbonOffsets;
+      let totalTCO2sHeld = parseEther("0.0");
+
+      await Promise.all(
+        scoredTCO2s.map(async (token) => {
+          // @ts-ignore
+          tokenContract = new ethers.Contract(token, tcoAbi.abi, owner);
+          const balance = await tokenContract.balanceOf(offsetHelper.address);
+          totalTCO2sHeld = totalTCO2sHeld.add(balance);
+        })
+      );
+
+      expect(formatEther(totalTCO2sHeld)).to.be.eql("0.0");
+    });
+
+    it("User's in-contract TCO2 balance should be 0.0 - offset from MATIC", async function () {
       const maticToSend = await offsetHelper.howMuchETHShouldISendToSwap(
         addresses.nctAddress,
         parseEther("1.0")
@@ -665,13 +723,45 @@ describe("Offset Helper", function () {
         )
       ).wait();
 
-      // TODO there should be a better more accurate way to test this, like maybe finding the total TCO2 supply
       expect(
         formatEther(await offsetHelper.tco2Balance(addresses.myAddress))
       ).to.be.eql("0.0");
     });
 
-    it("Should retire 1 TCO2 from NCT", async function () {
+    it("OffsetHelpers's TCO2 balances should be 0.0 - offset from MATIC", async function () {
+      const maticToSend = await offsetHelper.howMuchETHShouldISendToSwap(
+        addresses.nctAddress,
+        parseEther("1.0")
+      );
+
+      await (
+        await offsetHelper["autoOffset(address,uint256)"](
+          addresses.nctAddress,
+          parseEther("1.0"),
+          {
+            value: maticToSend,
+          }
+        )
+      ).wait();
+
+      const scoredTCO2s = await nct.getScoredTCO2s();
+
+      let tokenContract: ToucanCarbonOffsets;
+      let totalTCO2sHeld = parseEther("0.0");
+
+      await Promise.all(
+        scoredTCO2s.map(async (token) => {
+          // @ts-ignore
+          tokenContract = new ethers.Contract(token, tcoAbi.abi, owner);
+          const balance = await tokenContract.balanceOf(offsetHelper.address);
+          totalTCO2sHeld = totalTCO2sHeld.add(balance);
+        })
+      );
+
+      expect(formatEther(totalTCO2sHeld)).to.be.eql("0.0");
+    });
+
+    it("User's in-contract TCO2 balance should be 0.0 - offset from NCT", async function () {
       // since I have no NCT, I need to impersonate an account that has it
       // I'll also give it some wei, just to be safe
       await network.provider.request({
@@ -708,7 +798,6 @@ describe("Offset Helper", function () {
           )
       ).wait();
 
-      // TODO there should be a better more accurate way to test this, like maybe finding the total TCO2 supply
       expect(
         formatEther(
           await offsetHelper.tco2Balance(
@@ -716,6 +805,60 @@ describe("Offset Helper", function () {
           )
         )
       ).to.be.eql("0.0");
+    });
+
+    it("OffsetHelpers's TCO2 balances should be 0.0 - offset from NCT", async function () {
+      // since I have no NCT, I need to impersonate an account that has it
+      // I'll also give it some wei, just to be safe
+      await network.provider.request({
+        method: "hardhat_stopImpersonatingAccount",
+        params: [addresses.myAddress],
+      });
+      await network.provider.request({
+        method: "hardhat_impersonateAccount",
+        params: ["0xdab7f2bc9aa986d9759718203c9a76534894e900"],
+      });
+      await network.provider.send("hardhat_setBalance", [
+        "0xdab7f2bc9aa986d9759718203c9a76534894e900",
+        parseEther("2.0").toHexString(),
+      ]);
+      const signer = await ethers.getSigner(
+        "0xdab7f2bc9aa986d9759718203c9a76534894e900"
+      );
+
+      // @ts-ignore
+      nct = new ethers.Contract(addresses.nctAddress, nctAbi.abi, owner);
+
+      await (
+        await nct
+          .connect(signer)
+          .approve(offsetHelper.address, parseEther("1.0"))
+      ).wait();
+
+      await (
+        await offsetHelper
+          .connect(signer)
+          .autoOffsetUsingRedeemableToken(
+            addresses.nctAddress,
+            parseEther("1.0")
+          )
+      ).wait();
+
+      const scoredTCO2s = await nct.getScoredTCO2s();
+
+      let tokenContract: ToucanCarbonOffsets;
+      let totalTCO2sHeld = parseEther("0.0");
+
+      await Promise.all(
+        scoredTCO2s.map(async (token) => {
+          // @ts-ignore
+          tokenContract = new ethers.Contract(token, tcoAbi.abi, owner);
+          const balance = await tokenContract.balanceOf(offsetHelper.address);
+          totalTCO2sHeld = totalTCO2sHeld.add(balance);
+        })
+      );
+
+      expect(formatEther(totalTCO2sHeld)).to.be.eql("0.0");
     });
   });
 });
