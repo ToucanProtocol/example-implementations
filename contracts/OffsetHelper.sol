b04b9ec8a08963eff3dcc1ac1f6cf1d655a5473f
lazaralex98 <alex2012ro@gmail.com>
2022-03-10 15:17:25 +0200

    moved admin methods

--- contracts/OffsetHelper.sol
+++ contracts/OffsetHelper.sol
@@ -12,6 +12,29 @@ import "./uniswapv2/IUniswapV2Router02.sol";
 contract OffsetHelper is OffsetHelperStorage {
     using SafeERC20 for IERC20;
 
+    constructor() {
+        // TODO make these be assigned dynamically through the constructor
+        eligibleTokenAddresses[
+            "BCT"
+        ] = 0x2F800Db0fdb5223b3C3f354886d907A671414A7F;
+
+        eligibleTokenAddresses[
+            "NCT"
+        ] = 0xD838290e877E0188a4A44700463419ED96c16107;
+
+        eligibleTokenAddresses[
+            "USDC"
+        ] = 0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174;
+
+        eligibleTokenAddresses[
+            "WETH"
+        ] = 0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619;
+
+        eligibleTokenAddresses[
+            "WMATIC"
+        ] = 0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270;
+    }
+
     // @description this is the autoOffset method for when the user wants to input tokens like USDC, WETH, WMATIC
     // @param _depositedToken the address of the token that the user sends (could be USDC, WETH, WMATIC)
     // @param _carbonToken the pool that the user wants to use (could be NCT or BCT)
@@ -276,5 +299,39 @@ contract OffsetHelper is OffsetHelperStorage {
         // require the user has overallOffsetAmount > 0
         // mint NFT
         // reset overallOffsetAmount for user to 0
+    }
+
+    // ----------------------------------------
+    //  Admin methods
+    // ----------------------------------------
+
+    // @description you can use this to change or add eligible tokens and their addresses if needed
+    // @param _tokenSymbol symbol of the token to add
+    // @param _address the address of the token to add
+    function setEligibleTokenAddress(
+        string memory _tokenSymbol,
+        address _address
+    ) public virtual onlyOwner {
+        eligibleTokenAddresses[_tokenSymbol] = _address;
+    }
+
+    // @description you can use this to delete eligible tokens  if needed
+    // @param _tokenSymbol symbol of the token to add
+    function deleteEligibleTokenAddress(string memory _tokenSymbol)
+        public
+        virtual
+        onlyOwner
+    {
+        delete eligibleTokenAddresses[_tokenSymbol];
+    }
+
+    // @description you can use this to change the TCO2 contracts registry if needed
+    // @param _address the contract registry to use
+    function setToucanContractRegistry(address _address)
+        public
+        virtual
+        onlyOwner
+    {
+        contractRegistryAddress = _address;
     }
 }
