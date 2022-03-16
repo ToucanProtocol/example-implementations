76cb158b089fc2a4014e827f7efe43ef00081d82
lazaralex98 <alex2012ro@gmail.com>
2022-03-17 01:19:22 +0200

    chore: prepare deployment

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -9,8 +9,6 @@ import "solidity-coverage";
 
 dotenv.config();
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
 
@@ -18,9 +16,6 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
     console.log(account.address);
   }
 });
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
 
 const config: HardhatUserConfig = {
   defaultNetwork: "hardhat",
