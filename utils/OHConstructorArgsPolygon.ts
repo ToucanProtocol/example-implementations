01dc8ba917ccf91d4d73ca607004fb677a32a09b
Alex Lazar <alex2012ro@gmail.com>
2022-05-03 21:00:05 +0300

    chore: OffsetHelper redeployment & verification

---
+++ utils/OHConstructorArgsPolygon.ts
@@ -0,0 +1,13 @@
+import addresses from "./addresses";
+import { tokens } from "./tokens";
+
+module.exports = [
+  tokens,
+  [
+    addresses.bct,
+    addresses.nct,
+    addresses.usdc,
+    addresses.weth,
+    addresses.wmatic,
+  ],
+];
