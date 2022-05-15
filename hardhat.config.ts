ca53cffcac11079f2ba7d8eb2f68f70ca8aee6b5
Alex Lazar <alex2012ro@gmail.com>
2022-05-15 18:18:17 +0300

    chore: write tasks for OH
    Every time I make edits to the OffsetHelper I need to re-deploy and re-verify. I coded a task to make this process easier.
    
    fix: CI deployment
    
    I didn't notice the CI deployed on hardhat before. I edited the CI and the task such that it can deploy on hardhat.

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,11 +1,14 @@
 import * as dotenv from "dotenv";
 
-import { HardhatUserConfig, task } from "hardhat/config";
+import { HardhatUserConfig, subtask, task } from "hardhat/config";
 import "@nomiclabs/hardhat-etherscan";
 import "@nomiclabs/hardhat-waffle";
 import "@typechain/hardhat";
 import "hardhat-gas-reporter";
 import "solidity-coverage";
+import { tokens } from "./utils/tokens";
+import addresses, { mumbaiAddresses } from "./utils/addresses";
+import { network } from "hardhat";
 
 dotenv.config();
 
@@ -16,6 +19,41 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
     console.log(account.address);
   }
 });
+
+task("deployOffsetHelper", "Deploys and verifies OffsetHelper").setAction(
+  async (taskArgs, hre) => {
+    const OffsetHelper = await hre.ethers.getContractFactory("OffsetHelper");
+
+    const addressesToUse =
+      hre.network.name == "mumbai" ? mumbaiAddresses : addresses;
+
+    const oh = await OffsetHelper.deploy(tokens, [
+      addressesToUse.bct,
+      addressesToUse.nct,
+      addressesToUse.usdc,
+      addressesToUse.weth,
+      addressesToUse.wmatic,
+    ]);
+    await oh.deployed();
+    await oh.deployTransaction.wait(5);
+    console.log(`OffsetHelper deployed on ${hre.network.name} to:`, oh.address);
+
+    await hre.run("verify:verify", {
+      address: oh.address,
+      constructorArguments: [
+        tokens,
+        [
+          addressesToUse.bct,
+          addressesToUse.nct,
+          addressesToUse.usdc,
+          addressesToUse.weth,
+          addressesToUse.wmatic,
+        ],
+      ],
+    });
+    console.log(`OffsetHelper verified on ${hre.network.name} to:`, oh.address);
+  }
+);
 
 const config: HardhatUserConfig = {
   defaultNetwork: "hardhat",
@@ -33,16 +71,12 @@ const config: HardhatUserConfig = {
       url:
         process.env.POLYGON_URL || "https://matic-mainnet.chainstacklabs.com",
       accounts:
-        process.env.POLYGON_PRIVATE_KEY !== undefined
-          ? [process.env.POLYGON_PRIVATE_KEY]
-          : [],
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
     mumbai: {
       url: process.env.MUMBAI_URL || "https://matic-mumbai.chainstacklabs.com",
       accounts:
-        process.env.MUMBAI_PRIVATE_KEY !== undefined
-          ? [process.env.MUMBAI_PRIVATE_KEY]
-          : [],
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
     hardhat: {
       forking: {
