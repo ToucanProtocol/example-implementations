b56d6b031928779be39cccf69e7bee0c1a468b5b
Alex Lazar <alex2012ro@gmail.com>
2022-09-27 21:56:51 +0300

    test: unused imports, ts-ignores, not.reverted
    - removed unused imports
    - fixed ts-ignores
    - removed problematic `.not.be.reverted`
    - extracted parseEther("1.0") to a constant

--- test/OffsetHelper.test.ts
+++ test/OffsetHelper.test.ts
@@ -1,13 +1,7 @@
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { expect } from "chai";
-import { ethers, network } from "hardhat";
-import {
-  formatEther,
-  FormatTypes,
-  Interface,
-  parseEther,
-  parseUnits,
-} from "ethers/lib/utils";
+import { ethers } from "hardhat";
+import { formatEther, parseEther, parseUnits } from "ethers/lib/utils";
 
 import * as hardhatContracts from "../utils/toucanContracts.json";
 import * as poolContract from "../artifacts/contracts/interfaces/IToucanPoolToken.sol/IToucanPoolToken.json";
@@ -21,6 +15,8 @@ import {
 import addresses from "../utils/addresses";
 import { Contract } from "ethers";
 import { usdcABI, wethABI, wmaticABI } from "../utils/ABIs";
+
+const ONE_ETHER = parseEther("1.0");
 
 describe("Offset Helper - autoOffset", function () {
   let offsetHelper: OffsetHelper;
@@ -56,15 +52,17 @@ describe("Offset Helper - autoOffset", function () {
     wmatic = new ethers.Contract(addresses.wmatic, wmaticABI, addr2);
     usdc = new ethers.Contract(addresses.usdc, usdcABI, addr2);
 
-    // @ts-ignore
     nct = new ethers.Contract(
       addresses.nct,
       hardhatContracts.contracts.NatureCarbonTonne.abi,
       addr2
-    );
+    ) as IToucanPoolToken;
 
-    // @ts-ignore
-    bct = new ethers.Contract(addresses.bct, poolContract.abi, addr2);
+    bct = new ethers.Contract(
+      addresses.bct,
+      poolContract.abi,
+      addr2
+    ) as IToucanPoolToken;
   });
 
   before(async () => {
@@ -89,7 +87,7 @@ describe("Offset Helper - autoOffset", function () {
       addrs.map(async (addr) => {
         await addr.sendTransaction({
           to: addr2.address,
-          value: (await addr.getBalance()).sub(parseEther("1.0")),
+          value: (await addr.getBalance()).sub(ONE_ETHER),
         });
       })
     );
@@ -131,7 +129,7 @@ describe("Offset Helper - autoOffset", function () {
           await offsetHelper.calculateNeededTokenAmount(
             addresses.weth,
             addresses.nct,
-            parseEther("1.0")
+            ONE_ETHER
           )
         )
       ).wait();
@@ -140,7 +138,7 @@ describe("Offset Helper - autoOffset", function () {
         offsetHelper.autoOffsetUsingToken(
           addresses.weth,
           addresses.nct,
-          parseEther("1.0")
+          ONE_ETHER
         )
       ).to.not.be.reverted;
     });
@@ -148,22 +146,18 @@ describe("Offset Helper - autoOffset", function () {
     it("Should retire using a MATIC swap and NCT redemption", async function () {
       const maticToSend = await offsetHelper.calculateNeededETHAmount(
         addresses.nct,
-        parseEther("1.0")
+        ONE_ETHER
       );
 
-      await expect(
-        offsetHelper.autoOffsetUsingETH(addresses.nct, parseEther("1.0"), {
-          value: maticToSend,
-        })
-      ).to.not.be.reverted;
+      await offsetHelper.autoOffsetUsingETH(addresses.nct, ONE_ETHER, {
+        value: maticToSend,
+      });
     });
 
     it("Should retire using a NCT deposit and NCT redemption", async function () {
-      await (await nct.approve(offsetHelper.address, parseEther("1.0"))).wait();
+      await (await nct.approve(offsetHelper.address, ONE_ETHER)).wait();
 
-      await expect(
-        offsetHelper.autoOffsetUsingPoolToken(addresses.nct, parseEther("1.0"))
-      ).to.not.be.reverted;
+      await offsetHelper.autoOffsetUsingPoolToken(addresses.nct, ONE_ETHER);
     });
 
     it("Should retire using a WETH swap and BCT redemption", async function () {
@@ -173,18 +167,16 @@ describe("Offset Helper - autoOffset", function () {
           await offsetHelper.calculateNeededTokenAmount(
             addresses.weth,
             addresses.bct,
-            parseEther("1.0")
+            ONE_ETHER
           )
         )
       ).wait();
 
-      await expect(
-        offsetHelper.autoOffsetUsingToken(
-          addresses.weth,
-          addresses.bct,
-          parseEther("1.0")
-        )
-      ).to.not.be.reverted;
+      await offsetHelper.autoOffsetUsingToken(
+        addresses.weth,
+        addresses.bct,
+        ONE_ETHER
+      );
     });
 
     it("Should retire using a USDC swap and NCT redemption", async function () {
@@ -194,18 +186,16 @@ describe("Offset Helper - autoOffset", function () {
           await offsetHelper.calculateNeededTokenAmount(
             addresses.usdc,
             addresses.nct,
-            parseEther("1.0")
+            ONE_ETHER
           )
         )
       ).wait();
 
-      await expect(
-        offsetHelper.autoOffsetUsingToken(
-          addresses.usdc,
-          addresses.nct,
-          parseEther("1.0")
-        )
-      ).to.not.be.reverted;
+      await offsetHelper.autoOffsetUsingToken(
+        addresses.usdc,
+        addresses.nct,
+        ONE_ETHER
+      );
     });
 
     it("Should retire using a WMATIC swap and NCT redemption", async function () {
@@ -219,63 +209,51 @@ describe("Offset Helper - autoOffset", function () {
           await offsetHelper.calculateNeededTokenAmount(
             addresses.wmatic,
             addresses.nct,
-            parseEther("1.0")
+            ONE_ETHER
           )
         )
       ).wait();
 
-      await expect(
-        offsetHelper.autoOffsetUsingToken(
-          addresses.wmatic,
-          addresses.nct,
-          parseEther("1.0")
-        )
-      ).to.not.be.reverted;
+      await offsetHelper.autoOffsetUsingToken(
+        addresses.wmatic,
+        addresses.nct,
+        ONE_ETHER
+      );
     });
   });
 
   describe("Testing autoRedeem()", function () {
     it("Should redeem NCT", async function () {
-      await (await nct.approve(offsetHelper.address, parseEther("1.0"))).wait();
+      await (await nct.approve(offsetHelper.address, ONE_ETHER)).wait();
 
-      await (
-        await offsetHelper.deposit(addresses.nct, parseEther("1.0"))
-      ).wait();
+      await (await offsetHelper.deposit(addresses.nct, ONE_ETHER)).wait();
 
-      await expect(offsetHelper.autoRedeem(addresses.nct, parseEther("1.0"))).to
-        .not.be.reverted;
+      await offsetHelper.autoRedeem(addresses.nct, ONE_ETHER);
     });
 
     it("Should fail because we haven't deposited NCT", async function () {
       await expect(
-        offsetHelper.autoRedeem(addresses.nct, parseEther("1.0"))
+        offsetHelper.autoRedeem(addresses.nct, ONE_ETHER)
       ).to.be.revertedWith("Insufficient NCT/BCT balance");
     });
 
     it("Should redeem BCT", async function () {
-      // @ts-ignore
+      await (await bct.approve(offsetHelper.address, ONE_ETHER)).wait();
 
-      await (await bct.approve(offsetHelper.address, parseEther("1.0"))).wait();
+      await (await offsetHelper.deposit(addresses.bct, ONE_ETHER)).wait();
 
-      await (
-        await offsetHelper.deposit(addresses.bct, parseEther("1.0"))
-      ).wait();
-
-      await expect(offsetHelper.autoRedeem(addresses.bct, parseEther("1.0"))).to
-        .not.be.reverted;
+      await offsetHelper.autoRedeem(addresses.bct, ONE_ETHER);
     });
   });
 
   describe("Testing autoRetire()", function () {
     it("Should retire using an NCT deposit", async function () {
-      await (await nct.approve(offsetHelper.address, parseEther("1.0"))).wait();
+      await (await nct.approve(offsetHelper.address, ONE_ETHER)).wait();
 
-      await (
-        await offsetHelper.deposit(addresses.nct, parseEther("1.0"))
-      ).wait();
+      await (await offsetHelper.deposit(addresses.nct, ONE_ETHER)).wait();
 
       const redeemReceipt = await (
-        await offsetHelper.autoRedeem(addresses.nct, parseEther("1.0"))
+        await offsetHelper.autoRedeem(addresses.nct, ONE_ETHER)
       ).wait();
 
       if (!redeemReceipt.events) {
@@ -286,27 +264,25 @@ describe("Offset Helper - autoOffset", function () {
       const amounts =
         redeemReceipt.events[redeemReceipt.events.length - 1].args?.amounts;
 
-      await expect(offsetHelper.autoRetire(tco2s, amounts)).to.not.be.reverted;
+      await offsetHelper.autoRetire(tco2s, amounts);
     });
 
     it("Should fail because we haven't redeemed any TCO2", async function () {
       await expect(
         offsetHelper.autoRetire(
           ["0xb139C4cC9D20A3618E9a2268D73Eff18C496B991"],
-          [parseEther("1.0")]
+          [ONE_ETHER]
         )
       ).to.be.revertedWith("Insufficient TCO2 balance");
     });
 
     it("Should retire using an NCT deposit", async function () {
-      await (await bct.approve(offsetHelper.address, parseEther("1.0"))).wait();
+      await (await bct.approve(offsetHelper.address, ONE_ETHER)).wait();
 
-      await (
-        await offsetHelper.deposit(addresses.bct, parseEther("1.0"))
-      ).wait();
+      await (await offsetHelper.deposit(addresses.bct, ONE_ETHER)).wait();
 
       const redeemReceipt = await (
-        await offsetHelper.autoRedeem(addresses.bct, parseEther("1.0"))
+        await offsetHelper.autoRedeem(addresses.bct, ONE_ETHER)
       ).wait();
 
       if (!redeemReceipt.events) {
@@ -317,17 +293,15 @@ describe("Offset Helper - autoOffset", function () {
       const amounts =
         redeemReceipt.events[redeemReceipt.events.length - 1].args?.amounts;
 
-      await expect(offsetHelper.autoRetire(tco2s, amounts)).to.not.be.reverted;
+      await offsetHelper.autoRetire(tco2s, amounts);
     });
   });
 
   describe("Testing deposit() and withdraw()", function () {
     it("Should deposit 1.0 NCT", async function () {
-      await (await nct.approve(offsetHelper.address, parseEther("1.0"))).wait();
+      await (await nct.approve(offsetHelper.address, ONE_ETHER)).wait();
 
-      await (
-        await offsetHelper.deposit(addresses.nct, parseEther("1.0"))
-      ).wait();
+      await (await offsetHelper.deposit(addresses.nct, ONE_ETHER)).wait();
 
       expect(
         formatEther(await offsetHelper.balances(addr2.address, addresses.nct))
@@ -336,28 +310,22 @@ describe("Offset Helper - autoOffset", function () {
 
     it("Should fail to deposit because we have no NCT", async function () {
       await (
-        await nct
-          .connect(addrs[0])
-          .approve(offsetHelper.address, parseEther("1.0"))
+        await nct.connect(addrs[0]).approve(offsetHelper.address, ONE_ETHER)
       ).wait();
 
       await expect(
-        offsetHelper.connect(addrs[0]).deposit(addresses.nct, parseEther("1.0"))
+        offsetHelper.connect(addrs[0]).deposit(addresses.nct, ONE_ETHER)
       ).to.be.revertedWith("ERC20: transfer amount exceeds balance");
     });
 
     it("Should deposit and withdraw 1.0 NCT", async function () {
       const preDepositNCTBalance = await nct.balanceOf(addr2.address);
 
-      await (await nct.approve(offsetHelper.address, parseEther("1.0"))).wait();
+      await (await nct.approve(offsetHelper.address, ONE_ETHER)).wait();
 
-      await (
-        await offsetHelper.deposit(addresses.nct, parseEther("1.0"))
-      ).wait();
+      await (await offsetHelper.deposit(addresses.nct, ONE_ETHER)).wait();
 
-      await (
-        await offsetHelper.withdraw(addresses.nct, parseEther("1.0"))
-      ).wait();
+      await (await offsetHelper.withdraw(addresses.nct, ONE_ETHER)).wait();
 
       const postWithdrawNCTBalance = await nct.balanceOf(addr2.address);
 
@@ -367,11 +335,9 @@ describe("Offset Helper - autoOffset", function () {
     });
 
     it("Should fail to withdraw because we haven't deposited enough NCT", async function () {
-      await (await nct.approve(offsetHelper.address, parseEther("1.0"))).wait();
+      await (await nct.approve(offsetHelper.address, ONE_ETHER)).wait();
 
-      await (
-        await offsetHelper.deposit(addresses.nct, parseEther("1.0"))
-      ).wait();
+      await (await offsetHelper.deposit(addresses.nct, ONE_ETHER)).wait();
 
       await expect(
         offsetHelper.withdraw(addresses.nct, parseEther("2.0"))
@@ -379,11 +345,9 @@ describe("Offset Helper - autoOffset", function () {
     });
 
     it("Should deposit 1.0 BCT", async function () {
-      await (await bct.approve(offsetHelper.address, parseEther("1.0"))).wait();
+      await (await bct.approve(offsetHelper.address, ONE_ETHER)).wait();
 
-      await (
-        await offsetHelper.deposit(addresses.bct, parseEther("1.0"))
-      ).wait();
+      await (await offsetHelper.deposit(addresses.bct, ONE_ETHER)).wait();
 
       expect(
         formatEther(await offsetHelper.balances(addr2.address, addresses.bct))
@@ -401,7 +365,7 @@ describe("Offset Helper - autoOffset", function () {
           await offsetHelper.calculateNeededTokenAmount(
             addresses.weth,
             addresses.nct,
-            parseEther("1.0")
+            ONE_ETHER
           )
         )
       ).wait();
@@ -410,14 +374,14 @@ describe("Offset Helper - autoOffset", function () {
         await offsetHelper["swap(address,address,uint256)"](
           addresses.weth,
           addresses.nct,
-          parseEther("1.0")
+          ONE_ETHER
         )
       ).wait();
 
       // I expect the offsetHelper will have 1 extra NCT in its balance
       const balance = await nct.balanceOf(offsetHelper.address);
       expect(formatEther(balance)).to.be.eql(
-        formatEther(initialBalance.add(parseEther("1.0")))
+        formatEther(initialBalance.add(ONE_ETHER))
       );
 
       // I expect that the user should have his in-contract balance for NCT to be 1.0
@@ -429,17 +393,13 @@ describe("Offset Helper - autoOffset", function () {
     it("Should swap MATIC for 1.0 NCT", async function () {
       const maticToSend = await offsetHelper.calculateNeededETHAmount(
         addresses.nct,
-        parseEther("1.0")
+        ONE_ETHER
       );
 
       await (
-        await offsetHelper["swap(address,uint256)"](
-          addresses.nct,
-          parseEther("1.0"),
-          {
-            value: maticToSend,
-          }
-        )
+        await offsetHelper["swap(address,uint256)"](addresses.nct, ONE_ETHER, {
+          value: maticToSend,
+        })
       ).wait();
 
       const balance = await nct.balanceOf(offsetHelper.address);
@@ -453,17 +413,13 @@ describe("Offset Helper - autoOffset", function () {
 
       const maticToSend = await offsetHelper.calculateNeededETHAmount(
         addresses.nct,
-        parseEther("1.0")
+        ONE_ETHER
       );
 
       await (
-        await offsetHelper["swap(address,uint256)"](
-          addresses.nct,
-          parseEther("1.0"),
-          {
-            value: maticToSend.add(parseEther("0.5")),
-          }
-        )
+        await offsetHelper["swap(address,uint256)"](addresses.nct, ONE_ETHER, {
+          value: maticToSend.add(parseEther("0.5")),
+        })
       ).wait();
 
       const postSwapETHBalance = await offsetHelper.provider.getBalance(
@@ -479,9 +435,7 @@ describe("Offset Helper - autoOffset", function () {
 
     it("Should fail since we have no WETH", async function () {
       await (
-        await weth
-          .connect(addrs[0])
-          .approve(offsetHelper.address, parseEther("1.0"))
+        await weth.connect(addrs[0]).approve(offsetHelper.address, ONE_ETHER)
       ).wait();
 
       await expect(
@@ -490,7 +444,7 @@ describe("Offset Helper - autoOffset", function () {
           ["swap(address,address,uint256)"](
             addresses.weth,
             addresses.nct,
-            parseEther("1.0")
+            ONE_ETHER
           )
       ).to.be.revertedWith("ERC20: transfer amount exceeds balance");
     });
@@ -501,7 +455,7 @@ describe("Offset Helper - autoOffset", function () {
       const neededAmount = await offsetHelper.calculateNeededTokenAmount(
         addresses.weth,
         addresses.bct,
-        parseEther("1.0")
+        ONE_ETHER
       );
 
       await (await weth.approve(offsetHelper.address, neededAmount)).wait();
@@ -510,14 +464,14 @@ describe("Offset Helper - autoOffset", function () {
         await offsetHelper["swap(address,address,uint256)"](
           addresses.weth,
           addresses.bct,
-          parseEther("1.0")
+          ONE_ETHER
         )
       ).wait();
 
       // I expect the offsetHelper will have 1 extra BCT in its balance
       const balance = await bct.balanceOf(offsetHelper.address);
       expect(formatEther(balance)).to.be.eql(
-        formatEther(initialBalance.add(parseEther("1.0")))
+        formatEther(initialBalance.add(ONE_ETHER))
       );
 
       // I expect that the user should have his in-contract balance for BCT to be 1.0
